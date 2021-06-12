143c0d477eb29b89791871ac7aef98e02e8735a1
mamontovdn <mamontovdn@yandex.ru>
2021-06-12 16:21:19 +0300

    fixed pep8 and closed connection

--- main.py
+++ main.py
@@ -17,30 +17,30 @@ def main():
     )
     sql_query = pd.read_sql_query(
         """
-    WITH user_sessions AS (
-        SELECT s.visitor_id,
-               COUNT(s.visitor_id) row_n,
-               Max(s.date_time)    session_date_time
-        FROM sessions s
-                 JOIN communications c
-                      ON s.visitor_id = c.visitor_id AND
-                         s.site_id = c.site_id
-        WHERE s.date_time <= c.date_time
-        GROUP BY s.visitor_id)
-    SELECT c.communication_id,
-           c.site_id,
-           c.visitor_id,
-           c.date_time communication_date_time,
-           s.visitor_session_id,
-           us.session_date_time,
-           s.campaign_id,
-           us.row_n
-    FROM communications c
-    LEFT JOIN user_sessions us ON us.visitor_id = c.visitor_id
-    LEFT JOIN sessions s ON us.visitor_id = s.visitor_id AND
-                            us.session_date_time = s.date_time
-    ;
-    """,
+            WITH user_sessions AS (
+                SELECT s.visitor_id,
+                       COUNT(s.visitor_id) row_n,
+                       Max(s.date_time)    session_date_time
+                FROM sessions s
+                         JOIN communications c
+                              ON s.visitor_id = c.visitor_id AND
+                                 s.site_id = c.site_id
+                WHERE s.date_time <= c.date_time
+                GROUP BY s.visitor_id)
+            SELECT c.communication_id,
+                   c.site_id,
+                   c.visitor_id,
+                   c.date_time communication_date_time,
+                   s.visitor_session_id,
+                   us.session_date_time,
+                   s.campaign_id,
+                   us.row_n
+            FROM communications c
+            LEFT JOIN user_sessions us ON us.visitor_id = c.visitor_id
+            LEFT JOIN sessions s ON us.visitor_id = s.visitor_id AND
+                                    us.session_date_time = s.date_time
+            ;
+        """,
         conn,
     )
     df = pd.DataFrame(
@@ -57,6 +57,7 @@ def main():
         ],
     )
     df.to_csv("result.csv", index=False)
+    conn.close()
 
 
 if __name__ == "__main__":
